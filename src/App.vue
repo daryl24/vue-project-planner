2e8d2df7c20a6c7969a8d22c2a55533bedf7c5a3
Daryl <drj_cordova@yahoo.com>
2024-01-13 03:48:48 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  background: #eee;
+}
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+  background: #eee;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div v-id="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <single-project :project="project"></single-project>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleProject from "../components/SingleProject.vue";
+
+export default {
+  name: "Home",
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
+};
+</script>
